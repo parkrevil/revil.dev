58c6824eedc7915c958ef418223b03af75417dcf
박준형 <revil.com@gmail.com>
2023-05-10 01:57:57 +0900

    feat: base layout

--- src/libs/components/header.svelte
+++ src/libs/components/header.svelte
@@ -1,18 +1,46 @@
-<div class="navbar bg-base-100">
-  <div class="navbar-start">
-    <a href="/" class="btn btn-ghost normal-case text-xl">revil.dev</a>
-  </div>
+<script>
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Avatar,
+		Dropdown,
+		DropdownItem,
+		DropdownHeader,
+		DropdownDivider,
+    A,
+	} from 'flowbite-svelte';
+</script>
 
-  <div class="navbar-center flex">
-    <ul class="menu menu-horizontal px-1">
-      <li><a href="/">대시보드</a></li>
-      <li><a href="/profile">프로필</a></li>
-      <li><a href="/projects">프로젝트</a></li>
-      <li><a href="/ggabongs">까만 봉다리</a></li>
-    </ul>
-  </div>
+<Navbar let:toggle fluid={true}>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+			>revil.dev</span
+		>
+	</NavBrand>
 
-  <div class="navbar-end">
-    <a href="/sign-in" class="btn btn-ghost">로그인</a>
-  </div>
-</div>
+	<div class="flex items-center order-2">
+    <A href="/sign-in" class="text-sm">로그인</A>
+		<Avatar id="avatar-menu" src="/images/profile-picture-3.webp" />
+		<NavHamburger on:click={toggle} class1="w-full" />
+	</div>
+
+	<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+		<DropdownHeader>
+			<span class="block text-sm"> Bonnie Green </span>
+			<span class="block truncate text-sm font-medium"> name@flowbite.com </span>
+		</DropdownHeader>
+		<DropdownItem>Dashboard</DropdownItem>
+		<DropdownDivider />
+		<DropdownItem>Sign out</DropdownItem>
+	</Dropdown>
+
+	<NavUl>
+		<NavLi href="/" active={true}>대시보드</NavLi>
+		<NavLi href="/profile">프로필</NavLi>
+		<NavLi href="/projects">프로젝트</NavLi>
+		<NavLi href="/ggabongs">까만 봉다리</NavLi>
+	</NavUl>
+</Navbar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,6 @@
 
 <Header />
 
-<div class="absolute left-0 top-[4rem] w-full max-w-full h-[calc(100%-4rem)]">
+<div class="absolute left-0 top-[4.5rem] w-full max-w-full h-[calc(100%-4.5rem)] px-4">
 	<slot />
 </div>
