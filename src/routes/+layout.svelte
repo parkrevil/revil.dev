ada5351a9310d62c68c55d1f3bb5779c28634d21
박준형 <revil.com@gmail.com>
2023-05-10 00:04:39 +0900

    feat: 기본 페이지 라우트

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<a href="/">대시보드</a>
+<a href="/profile">프로필</a>
+<a href="/projects">프로젝트</a>
+<a href="/ggabongs">까만 봉다리</a>
+<a href="/sign-in">로그인</a>
+<a href="/sign-up">계정만들기</a>
+
+
+<slot></slot>
