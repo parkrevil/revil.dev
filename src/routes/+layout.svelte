02dcae1761ec9e6da2be6ce5a5f4a0bdf937951c
박준형 <revil.com@gmail.com>
2023-05-10 00:18:13 +0900

    feat: Tailwindcss, DaisyUI 적용

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script>
+  import '../app.css';
+</script>
+
 <a href="/">대시보드</a>
 <a href="/profile">프로필</a>
 <a href="/projects">프로젝트</a>
@@ -5,5 +9,6 @@
 <a href="/sign-in">로그인</a>
 <a href="/sign-up">계정만들기</a>
 
+<input type="range" min="0" max="100" value="40" class="range range-primary" />
 
 <slot></slot>
