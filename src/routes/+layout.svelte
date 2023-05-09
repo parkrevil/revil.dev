5f317e88d1c10bd7e935e913898aeafa31e90c92
박준형 <revil.com@gmail.com>
2023-05-10 00:38:28 +0900

    feat: 헤더

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,9 @@
 <script>
   import '../app.css';
+	import Header from '../libs/components/header.svelte';
 </script>
 
-<a href="/">대시보드</a>
-<a href="/profile">프로필</a>
-<a href="/projects">프로젝트</a>
-<a href="/ggabongs">까만 봉다리</a>
-<a href="/sign-in">로그인</a>
-<a href="/sign-up">계정만들기</a>
+<Header></Header>
 
 <input type="range" min="0" max="100" value="40" class="range range-primary" />
 
